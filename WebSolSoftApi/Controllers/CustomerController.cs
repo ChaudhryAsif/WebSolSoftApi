7b1a4a8c0e80d4b60ced4d2bc9de69c915f12744
localhost <localhost@LOCALHOST>
2024-07-05 19:06:35 +0500

    Add project files.

---
+++ WebSolSoftApi/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSolSoftApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ILogger<CustomerController> _logger;
+
+        public CustomerController(ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetAll()
+        {
+            var customers = new List<string>() { "Asif", "Saleem" };
+            return Ok(customers);
+        }
+    }
+}
